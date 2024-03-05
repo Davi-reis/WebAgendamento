85c5bc8d412abc26037571b051a5a7d88480ef4d
davi-reis <reisdavi@hotmail.com>
2024-03-05 09:28:03 -0300

    #Correção da classe index e inclusão do carousel com imagens

---
+++ Pages/Shared/_Carousel.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebAgendamento.Pages.Shared
+{
+    public class _CarouselModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
